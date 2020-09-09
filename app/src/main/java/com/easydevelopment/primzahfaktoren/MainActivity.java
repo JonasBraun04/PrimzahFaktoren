256f6776c2048e214441829c8a2baaef6e65eab3
IsiTapier <41296026+IsiTapier@users.noreply.github.com>
2020-09-09 17:39:14 +0200

    a

--- app/src/main/java/com/easydevelopment/primzahfaktoren/MainActivity.java
+++ app/src/main/java/com/easydevelopment/primzahfaktoren/MainActivity.java
@@ -2,60 +2,91 @@ package com.easydevelopment.primzahfaktoren;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    EditText input;
+    TextView output;
+    CheckBox toggleMode;
+    String outputText;
+    int i;
+    int counter;
 
-ListView list;
-EditText input;
-Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         input = findViewById(R.id.inputNumber);
-        bt = findViewById(R.id.button);
-        bt.setOnClickListener(new View.OnClickListener() {
+        output = findViewById(R.id.output);
+        toggleMode = findViewById(R.id.potenzMode);
+
+        input.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                algorithmus a = new algorithmus();
-                ArrayList res = a.calc(Integer.parseInt(input.getText().toString()));
-                list  = (ListView)findViewById(R.id.list);
-                ListAdapter adapter = new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1,res);
-                list.setAdapter(adapter);
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updateDisplay();
             }
         });
-        while(true) {
-            int old = 0;
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+        toggleMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                updateDisplay();
             }
-           //if (input.isInEditMode()) {
-           //    int New = input.getText().toString().length();
-           //    if (New != old) {
-           //        New = old;
-           //        Log.d("test", "ft");
-
-           //    }
-           //}
-        }
-
-
-
+        });
     }
 
+    public void updateDisplay() {
+        outputText = "";
+        if (input.getText().length() != 0) {
+            PrimeFactorsCalculation primeFactorsCalculation = new PrimeFactorsCalculation();
+            ArrayList result = primeFactorsCalculation.calculation(Integer.parseInt(input.getText().toString()));
+            counter = 1;
+            if (toggleMode.isChecked()) {
+                for (i = 0; i < result.size()-1; i++) {
+                    if (result.get(i) == result.get(i + 1)) {
+                        counter++;
+                    } else {
+                        outputAdd(false, result);
+                    }
+                }
+            } else {
+                for (i = 0; i < result.size()-1; i++) {
+                    outputAdd(false, result);
+                }
+            }
+            outputAdd(true, result);
+        }
+        output.setText(outputText);
+    }
 
+    public void outputAdd(boolean end, ArrayList array) {
+        if (counter > 1) {
+            outputText = outputText + array.get(i) + "^" + counter;
+            counter = 1;
+        } else {
+            outputText = outputText + array.get(i) + "";
+        }
+        if(!end) {
+            outputText = outputText + " x ";
+        }
+    }
 }
